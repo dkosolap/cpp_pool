b81dfb7d8a7e7d97a392b588ffe2b9934aa26e23
Dmitry Kosolap <dkosolap@e1r9p7.unit.ua>
2018-07-16 17:09:34 +0300

    rewrite 3day/ex00

--- 3day/ex00/FragTrap.hpp
+++ 3day/ex00/FragTrap.hpp
@@ -1,34 +1,36 @@
-#ifndef FRAGTRAP_CLASS_H
-# define FRAGTRAP_CLASS_H 
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP 
 # include <iostream>
 
 class FragTrap {
+	unsigned int			_hit;
+	unsigned int			_maxHit;
+	unsigned int			_maxEnergy;
+	unsigned int			_energy;
+	unsigned int			_level;
+	unsigned int			_meelAttack;
+	unsigned int			_rangedAttack;
+	unsigned int			_armorReduction;
+	std::string				_name;
 
-    private:
-        unsigned int    _hitPoint;
-        unsigned int    _energyPoint;
-        std::string     _name;
 
-    public:
-        static unsigned int const   maxHitPoint;
-        static unsigned int const   maxEnergyPoint;
-        static unsigned int const   level;
+public:
+	FragTrap( void );
+	FragTrap( FragTrap const &ref );
+	FragTrap( std::string name );
+	~FragTrap( void );
 
-        unsigned int const          meelDamage;
-        unsigned int const          rangeDamage;
-        unsigned int const          armorDamage;
-// method
-    private:
+	std::string	getName( void ) const;
+	void		rangedAttack( std::string const & target );
+	void		meleeAttack( std::string const & target );
+	void		takeDamage( unsigned int amount );
+	void		beRepaired( unsigned int amount );
+	void		vaulthunter_dot_exe( std::string const & target );
 
-    public:
-    FragTrap(std::string const & name);
-    ~FragTrap( void );
-    void    rangedAttack(std::string const & target);
-    void    melleAttack(std::string const & target);
-    void    takeDamage(unsigned int amount);
-    void    beRepaired(unsigned int amount);
-    void    vaulthunter_dot_exe(std::string const & target);
-    std::string getName( void );
+	FragTrap	&operator=( FragTrap const &ref );
+	
 };
+
+std::ostream	&operator<<( std::ostream &os, FragTrap const &ref );
 
 #endif
--- 3day/ex00/main.cpp
+++ 3day/ex00/main.cpp
@@ -1,30 +1,29 @@
 #include "FragTrap.hpp"
 
-int main( void ) {
+int	main() {
 
-    
-    FragTrap    *lalka = new FragTrap("lalka");
+	srand(time( 0 ));
+	FragTrap    *lalka = new FragTrap("lalka");
 
-    srand( time( 0 ) );
-    lalka->vaulthunter_dot_exe("PRO");
-    lalka->rangedAttack("PRO");
-    lalka->melleAttack("PRO");
-    lalka->vaulthunter_dot_exe("PRO");
-    lalka->vaulthunter_dot_exe("PRO");
-    lalka->vaulthunter_dot_exe("PRO");
-    lalka->takeDamage(10);
-    lalka->takeDamage(10);
-    lalka->takeDamage(10);
-    lalka->takeDamage(10);
-    lalka->takeDamage(100);
-    lalka->beRepaired(15);
-    lalka->beRepaired(15);
-    lalka->beRepaired(15);
-    lalka->beRepaired(15);
-    lalka->beRepaired(15);
-    lalka->beRepaired(100);
-    lalka->beRepaired(15);
+	lalka->vaulthunter_dot_exe("PRO");
+	lalka->rangedAttack("PRO");
+	lalka->meleeAttack("PRO");
+	lalka->vaulthunter_dot_exe("PRO");
+	lalka->vaulthunter_dot_exe("PRO");
+	lalka->vaulthunter_dot_exe("PRO");
+	lalka->takeDamage(10);
+	lalka->takeDamage(10);
+	lalka->takeDamage(10);
+	lalka->takeDamage(10);
+	lalka->takeDamage(100);
+	lalka->beRepaired(15);
+	lalka->beRepaired(15);
+	lalka->beRepaired(15);
+	lalka->beRepaired(15);
+	lalka->beRepaired(15);
+	lalka->beRepaired(100);
+	lalka->beRepaired(15);
 
-    delete(lalka);
-    return 0;
+	delete(lalka);
+	return (0);
 }
